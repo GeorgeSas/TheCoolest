9b2714d6c5313d93e107e2b7b52148175017fdcc
George <georgian.sas@gmail.com>
2015-11-15 23:26:39 +0200

    Push eclipse project

---
+++ src/projectForGit/SimpleApp.java
@@ -0,0 +1,16 @@
+package projectForGit;
+
+public class SimpleApp {
+
+	public static void main(String[] args) {
+		Integer firstNum = 3;
+		Integer secondNum = 5;
+		
+		if (firstNum > secondNum) {
+			System.out.println("Seems to me all is good.");	
+		} else {
+			System.out.println("Conditions is flase, so here we are.");
+		}
+	}
+
+}
