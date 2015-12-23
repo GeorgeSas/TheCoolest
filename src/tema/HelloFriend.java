974260388b2e275df4bafec3556083fc7b1bd09f
silvia <s00kie9@yahoo.com>
2015-12-23 21:49:20 +0200

    trying to push to github

---
+++ src/tema/HelloFriend.java
@@ -0,0 +1,15 @@
+package tema;
+
+public class HelloFriend {
+	private String name;
+	public void setName(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	public static void main(String args[]) {
+		HelloFriend javaClassExample = new HelloFriend();
+		System.out.println("Hello " + javaClassExample.getName());
+	}
+}
